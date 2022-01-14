9ca9f0764d54948d0d7c63e54323289bb776c535
baptistefray <baptiste.fray@edu.ece.fr>
2022-01-14 09:14:32 +0100

    Create projet Dancing Links + add reference to Benchmark

---
+++ Sudoku.DancingLinksSolvers/DancingLinksSolver.cs
@@ -0,0 +1,14 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.DancingLinksSolvers{
+    public class DancingLinksSolvers1 : ISolverSudoku
+{
+    public Shared.GridSudoku Solve(Shared.GridSudoku s)
+        {
+            return s;
+            //  z3Context.MkTactic("smt");
+
+    }
+}
+
+}
