33561c544dad751ba910107b94293ca1f9dd7d67
Maxime RIVE <45031823+Vipeerz@users.noreply.github.com>
2022-01-14 09:39:21 +0100

    tient antoine

---
+++ Soduko.BacktrackingSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Soduko.BacktrackingSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
