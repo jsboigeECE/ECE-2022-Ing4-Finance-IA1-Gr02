602de0d2c638a44211c14ba420c4bc2880b0ac7e
François <francois.zhuge@gmail.com>
2022-01-14 12:05:45 +0100

    OR-Tools

---
+++ ConsoleApp1/Sudoku.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Sudoku
+    {
+        String etat;
+        String file;
+        static void Main(string[] args)
+        {
+            public String toString()
+            { 
+                return Sudoku.etat;
+            }
+             static public Sudoku ParseFile(String filePath)
+            {
+                new Sudoku[];
+
+
+            }
+                
+        }
+    }
+}
