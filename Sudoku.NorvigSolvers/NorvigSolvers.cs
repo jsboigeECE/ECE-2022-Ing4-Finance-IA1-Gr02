a9c5c18e3b13f66cc2f6bc4c0fe79cb204bd5242
SamiQuintana <57910155+SamiQuintana@users.noreply.github.com>
2022-01-14 09:28:57 +0100

    Add Norvig project to the Sudoku solution

---
+++ Sudoku.NorvigSolvers/NorvigSolvers.cs
@@ -0,0 +1,15 @@
+﻿using Sudoku.Shared;
+using System;
+
+namespace Sudoku.NorvigSolvers
+{
+    public class NorvigSolvers : ISolverSudoku
+    {
+        public Shared.GridSudoku Solve(Shared.GridSudoku s)
+        {
+            return s;
+            //  z3Context.MkTactic("smt");
+
+        }
+    }
+}
