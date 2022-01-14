1f37aaf9390988a3d36664a98653143d788bcd63
baptistefray <baptiste.fray@edu.ece.fr>
2022-01-14 11:28:07 +0100

    Code Dancing Links no error,doesn't work for solution

--- Sudoku.DancingLinksSolvers/DancingLinksSolver.cs
+++ Sudoku.DancingLinksSolvers/DancingLinksSolver.cs
@@ -10,15 +10,14 @@ namespace Sudoku.DancingLinksSolvers{
                 var internalRows = BuildInternalRowsForGrid(s);
                 var dlxRows = BuildDlxRows(internalRows);
                 var solutions = new Dlx()
-                    .Solve(dlxRows, d => d, r => r);
-                    //.Where(solution => VerifySolution(internalRows, solution))
-                    //.ToImmutableList();
+                    .Solve(dlxRows, d => d, r => r)
+                    .Where(solution => VerifySolution(internalRows, solution))
+                    .ToImmutableList();
 
                 Console.WriteLine();
 
                 if (solutions.Any())
                 {
-                    Console.WriteLine($"First solution (of {solutions.Count}):");
                     return s;
                 }
                 else
@@ -29,6 +28,20 @@ namespace Sudoku.DancingLinksSolvers{
                 
                 //  z3Context.MkTactic("smt");
 
+        }
+
+        private static IEnumerable<int> Rows => Enumerable.Range(0, 9);
+        private static IEnumerable<int> Cols => Enumerable.Range(0, 9);
+        private static IEnumerable<Tuple<int, int>> Locations =>
+            from row in Rows
+            from col in Cols
+            select Tuple.Create(row, col);
+        private static IEnumerable<int> Digits => Enumerable.Range(1, 9);
+
+        private static IImmutableList<IImmutableList<int>> BuildDlxRows(
+            IEnumerable<Tuple<int, int, int, bool>> internalRows)
+        {
+            return internalRows.Select(BuildDlxRow).ToImmutableList();
         }
         private static IImmutableList<int> BuildDlxRow(Tuple<int, int, int, bool> internalRow)
         {
@@ -62,6 +75,72 @@ namespace Sudoku.DancingLinksSolvers{
 
             return Digits.Select(v => Tuple.Create(row, col, v, false)).ToImmutableList();
         }
-    }
 
+        private static int RowColToBox(int row, int col)
+        {
+            return row - (row%3) + (col/3);
+        }
+
+        private static IEnumerable<int> Encode(int major, int minor)
+        {
+            var result = new int[81];
+            result[major*9 + minor] = 1;
+            return result.ToImmutableList();
+        }
+
+        private static bool VerifySolution(
+            IReadOnlyList<Tuple<int, int, int, bool>> internalRows,
+            Solution solution)
+        {
+            var solutionInternalRows = solution.RowIndexes
+                .Select(rowIndex => internalRows[rowIndex])
+                .ToImmutableList();
+
+            var locationsGroupedByRow = Locations.GroupBy(t => t.Item1);
+            var locationsGroupedByCol = Locations.GroupBy(t => t.Item2);
+            var locationsGroupedByBox = Locations.GroupBy(t => RowColToBox(t.Item1, t.Item2));
+
+            return
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByRow, "row") &&
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByCol, "col") &&
+                CheckGroupsOfLocations(solutionInternalRows, locationsGroupedByBox, "box");
+        }
+
+        private static bool CheckGroupsOfLocations(
+            IEnumerable<Tuple<int, int, int, bool>> solutionInternalRows,
+            IEnumerable<IGrouping<int, Tuple<int, int>>> groupedLocations,
+            string tag)
+        {
+            return groupedLocations.All(grouping =>
+                CheckLocations(solutionInternalRows, grouping, grouping.Key, tag));
+        }
+
+        private static bool CheckLocations(
+            IEnumerable<Tuple<int, int, int, bool>> solutionInternalRows,
+            IEnumerable<Tuple<int, int>> locations,
+            int key,
+            string tag)
+        {
+            var digits = locations.SelectMany(location =>
+                solutionInternalRows
+                    .Where(solutionInternalRow =>
+                        solutionInternalRow.Item1 == location.Item1 &&
+                        solutionInternalRow.Item2 == location.Item2)
+                    .Select(t => t.Item3));
+            return CheckDigits(digits, key, tag);
+        }
+
+        private static bool CheckDigits(
+            IEnumerable<int> digits,
+            int key,
+            string tag)
+        {
+            var actual = digits.OrderBy(v => v);
+            if (actual.SequenceEqual(Digits)) return true;
+            var values = string.Concat(actual.Select(n => Convert.ToString(n)));
+            Console.WriteLine($"{tag} {key}: {values} !!!");
+            return false;
+        }
+
+    }
 }
