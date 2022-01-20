12b4d2ac8931d7a6c320eeed95bba50df1e5de8e
MaximeR <maxime.rive@edu.ece.fr>
2022-01-20 17:48:09 +0100

    Wouhou Backtracking fiesta

--- Soduko.BacktrackingSolver/BacktrackingSolver1.cs
+++ Soduko.BacktrackingSolver/BacktrackingSolver1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft;
+using Google.Type;
 using Sudoku.Shared;
 
 namespace Soduko.BacktrackingSolver
@@ -9,12 +12,106 @@ namespace Soduko.BacktrackingSolver
 
         public BacktrackingSolver1()
         {
-
+           
         }
 
         public GridSudoku Solve(GridSudoku s)
         {
+            int[,] sudoku;
+
+            //Méthode pour utiliser un tableau format int[,] au lieu de [][] imposé par le format de base
+            //On créer donc un tableau int[,] qui prend toutes les valeurs de la grille de sudoku en paramètre
+            sudoku = Convertion(s);
+
+            //Appel de la méthode de résolution
+            SolverBacktracking(sudoku, 0, 0);
+
+            //Boucle pour mettre à jour le tableau du suduko à retourner à partir du tableau sur lequel on a fait les modifications
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    s.Cellules[i][j] = sudoku[i, j];
+                               
+
             return s;
         }
+
+
+        public int[,] Convertion(GridSudoku s)
+        {
+
+            int[,] sudok = new int[10, 10];
+
+            //On remplace chaque case du nouveau tableau par la grille passée en paramètre
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    sudok[i, j] = s.Cellules[i][j];
+
+            return sudok;
+        }
+
+        static bool SolverBacktracking(int[,] grid, int row,
+                          int col)
+        {
+
+           
+            if (row == 9 - 1 && col == 9)
+                return true;
+
+            
+            if (col == 9)
+            {
+                row++;
+                col = 0;
+            }
+
+         
+            if (grid[row, col] != 0)
+                return SolverBacktracking(grid, row, col + 1);
+
+            for (int num = 1; num < 10; num++)
+            { 
+                if (IsSafe(grid, row, col, num))
+                {
+
+                   
+                    grid[row, col] = num;
+
+                    
+                    if (SolverBacktracking(grid, row, col + 1))
+                        return true;
+                }
+             
+                grid[row, col] = 0;
+            }
+            return false;
+        }
+
+        static bool IsSafe(int[,] grid, int row, int col,
+                     int num)
+        {
+
+
+            for (int x = 0; x <= 8; x++)
+                if (grid[row, x] == num)
+                    return false;
+
+
+            for (int x = 0; x <= 8; x++)
+                if (grid[x, col] == num)
+                    return false;
+
+
+            int startRow = row - row % 3, startCol
+              = col - col % 3;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (grid[i + startRow, j + startCol] == num)
+                        return false;
+
+            return true;
+        }
+
+
+
     }
 }
