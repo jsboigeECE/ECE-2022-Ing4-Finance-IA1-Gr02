190cfb59e0d241243504035e51b7a2df61566742
Lilian Biscarrat <lilian.bscr@gmail.com>
2022-01-21 11:36:21 +0100

    sudokuFitness

--- Sudoku.GeneticSharpsSolver/SudokuFitness.cs
+++ Sudoku.GeneticSharpsSolver/SudokuFitness.cs
@@ -37,12 +37,12 @@ namespace Sudoku.GeneticSharpsSolver
         public double Evaluate(GridSudoku testGridSudoku)
         {
             // We use a large lambda expression to count duplicates in rows, columns and boxes
-            var cells = testGridSudoku.Cells.Select((c, i) => new { index = i, cell = c }).ToList();
+            var cells = testGridSudoku.Cellules.Select((c, i) => new { index = i, cell = c }).ToList();
             var toTest = cells.GroupBy(x => x.index / 9).Select(g => g.Select(c => c.cell)) // rows
               .Concat(cells.GroupBy(x => x.index % 9).Select(g => g.Select(c => c.cell))) //columns
               .Concat(cells.GroupBy(x => x.index / 27 * 27 + x.index % 9 / 3 * 3).Select(g => g.Select(c => c.cell))); //boxes
             var toReturn = -toTest.Sum(test => test.GroupBy(x => x).Select(g => g.Count() - 1).Sum()); // Summing over duplicates
-            toReturn -= cells.Count(x => _targetGridSudoku.Cells[x.index] > 0 && _targetGridSudoku.Cells[x.index] != x.cell); // Mask
+            toReturn -= cells.Count(x => _targetGridSudoku.Cellules[x.index][] > 0 && _targetGridSudoku.Cellules[x.index] != x.cell); // Mask
             return toReturn;
         }
     }
