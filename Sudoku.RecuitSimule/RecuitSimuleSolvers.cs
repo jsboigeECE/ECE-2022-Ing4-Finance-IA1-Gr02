07d189bd5ee589e47b31bf4dd50bfbfdfb227c1f
HippolyteAvit <hippolyte01@hotmail.fr>
2022-01-24 15:15:34 +0100

    Push Recuit Simulé Python
    Problème de format pour la solution du sudoku

--- Sudoku.RecuitSimule/RecuitSimuleSolvers.cs
+++ Sudoku.RecuitSimule/RecuitSimuleSolvers.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using Sudoku.Shared;
+using Python.Runtime;
+using System.Linq;
 
 namespace Sudoku.RecuitSimule
 {
-    public class RecuitSimuleSolvers : ISolverSudoku
+
+    public class RecuitSimuleSolvers : PythonSolverBase
     {
-        public GridSudoku Solve(GridSudoku s)
+
+        public override Shared.GridSudoku Solve(Shared.GridSudoku s)
         {
-            return s;
+
+            // Create Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cellules.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("instance", pyCells);
+
+                // the person object may now be used in Python
+                string code = Resources.RecuitSimuleSolver_py;
+                scope.Exec(code);
+                var result = scope.Get("solution");
+                var strResult = result.ToString();
+                var managedResult = strResult.Split("\n").Select(l=>l.Select(c => (int.Parse(c.ToString()))).ToArray()).ToArray(); //result.As < int[,]>();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+
+                return new Shared.GridSudoku() { Cellules = managedResult};
+            }
+            //}
+
+        }
+
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("numpy");
+            base.InitializePythonComponents();
         }
     }
 }
