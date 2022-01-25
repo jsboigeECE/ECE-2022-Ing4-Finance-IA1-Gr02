012bd799d20cb5625c71484b6336ae9c264866fd
baptistefray <baptiste.fray@edu.ece.fr>
2022-01-25 11:52:56 +0100

    Ajout Nouvelle Solution améliorer + Class Matrix

---
+++ Sudoku.DancingLinksSolvers/MatrixClass/MatrixNodeHead.cs
@@ -0,0 +1,15 @@
+namespace Sudoku.DancingLinksSolvers
+{
+    class MatrixNodeHead : MatrixNode
+    {
+        internal string name;
+        internal new MatrixNode item;
+        internal int size;
+
+        public MatrixNodeHead(string s) : base(null)
+        {
+            name = s;
+        }
+
+    }
+}
